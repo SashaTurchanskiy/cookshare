cb6a57458e7f6ff88c53d07cbb46bdbc386d8b49
Sasha Turchanskiy <turchanskiy777@gmail.com>
2025-04-29 16:20:43 +0300

    finished entity part

--- src/main/java/com/daylicodework/cookshare/model/Like.java
+++ src/main/java/com/daylicodework/cookshare/model/Like.java
@@ -11,6 +11,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "likes")
 public class Like {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
